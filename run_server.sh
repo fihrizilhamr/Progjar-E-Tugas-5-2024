007f2040e5aa147e6b00710828c8d59a587fa667
Fihriz Ilham Rabbany <116176265+fihrizilhamr@users.noreply.github.com>
2024-06-26 19:46:06 +0700

    Add files via upload

---
+++ run_server.sh
@@ -0,0 +1,8 @@
+python3 server_process_pool_http.py 8000 &
+python3 server_process_pool_http.py 8001 &
+python3 server_process_pool_http.py 8002 &
+python3 server_process_pool_http.py 8003 &
+
+
+
+
